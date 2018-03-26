806a8b2f58e4ec19582520d0f0a5dc5e31172a15
Kin Chan <xkinchanx@gmail.com>
2018-03-26 16:20:28 -0700

    [update]: partial conversion to react-native.   Commented out dependencies and functions ATM

--- client/src/components/buttons/FriendChat.jsx
+++ client/src/components/buttons/FriendChat.jsx
@@ -1,107 +1,166 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import io from 'socket.io-client/dist/socket.io';
-import Chat from './Chat';
-import store from '../../redux/store';
+import { ScrollView, StyleSheet, FlatList, Text, TouchableOpacity, View } from 'react-native';
 
-import './FriendChat.css';
-import location from '../../../../config';
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import io from 'socket.io-client/dist/socket.io';
+// import Chat from './Chat';
+// import store from '../../redux/store';
 
-class FriendChat extends Component {
+// import './FriendChat.css';
+// import location from '../../../../config';
+
+export default class Friends extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      friendsList: [],
+      friendsList: [
+        ['Joker', 'one'], 
+        ['Batman', 'two'], 
+        ['Robin', 'three'],
+        ['Clayface', 'four'],
+        ['Harley Q', 'five'], 
+        ['Solomon G', 'six'],
+        ['Azrael', 'six'],
+      ],
       socket: null,
       selectedChat: null,
-      store: store.getState(),
+      showFriends: false,
+      // store: store.getState(),
       setInactive: {},
     };
-    store.subscribe(() => {
-      this.state = store.getState();
-    });
+    // store.subscribe(() => {
+    //   this.state = store.getState();
+    // });
   }
 
-  componentDidMount() {
-    axios
-      .get('/api/user/friend')
-      .then(({ data }) => {
-        const friendsList = [];
-        data.forEach(friend =>
-          friendsList.push([friend.name, friend.roomID]));
-        this.setState({ friendsList }) // eslint-disable-line
-      })
-      .catch(err => console.log('FriendChat componentMount error: ', err));
+  // componentDidMount() {
+  //   axios
+  //     .get('/api/user/friend')
+  //     .then(({ data }) => {
+  //       const friendsList = [];
+  //       data.forEach(friend =>
+  //         friendsList.push([friend.name, friend.roomID]));
+  //       this.setState({ friendsList }) // eslint-disable-line
+  //     })
+  //     .catch(err => console.log('FriendChat componentMount error: ', err));
 
-    this.socket = io(`http://${location}:3444`, {
-      query: {
-        roomId: 'lobby',
-      },
-    });
-
-    this.socket.on('server.inLobby', (payload) => {
-      const domElement = document.getElementsByClassName(payload);
-      if (domElement.length > 0) {
-        domElement[0].style.backgroundColor = '#0EFF2E';
-        const { setInactive } = this.state;
-        if (setInactive[payload]) {
-          clearTimeout(setInactive[payload]);
-        }
-        setInactive[payload] = setTimeout(() => { domElement[0].style.backgroundColor = '#bbb'; }, 20000);
-        this.setState({ setInactive });
-      }
-    });
-
-    this.setState( { socket: this.socket }); // eslint-disable-line
-  }
-
-  changeSelectedChat(friendName, roomID) {
-    this.setState({ selectedChat: false });
-    setTimeout(() => {
-      this.setState({ selectedChat: [friendName, roomID] });
-      document.getElementById('selectedChat').classList.remove('hide');
-    }, 0);
-  }
-
-  openFriendList(e) {
-    e.preventDefault();
-    document.getElementById("friendList").style.height = "200px";
-
-    const { socket } = this.state;
-    socket.emit('client.inLobby', this.state.store.user);
-  }
-
-  closeFriendList() {
-    document.getElementById("friendList").style.height = "0";
-    this.setState({ selectedChat: false });
-  }
+  //   this.socket = io(`http://${location}:3444`, {
+  //     query: {
+  //       roomId: 'lobby',
+  //     },
+  //   });
   
+  //   this.socket.on('server.inLobby', (payload) => {
+  //     const domElement = document.getElementsByClassName(payload);
+  //     if (domElement.length > 0) {
+  //       domElement[0].style.backgroundColor = '#0EFF2E';
+  //       const { setInactive } = this.state;
+  //       if (setInactive[payload]) {
+  //         clearTimeout(setInactive[payload]);
+  //       }
+  //       setInactive[payload] = setTimeout(() => { domElement[0].style.backgroundColor = '#bbb'; }, 20000);
+  //       this.setState({ setInactive });
+  //     }
+  //   });
+
+  //   this.setState( { socket: this.socket }); // eslint-disable-line
+  // }
+
+  // changeSelectedChat(friendName, roomID) {
+  //   this.setState({ selectedChat: false });
+  //   setTimeout(() => {
+  //     this.setState({ selectedChat: [friendName, roomID] });
+  //     document.getElementById('selectedChat').classList.remove('hide');
+  //   }, 0);
+  // }
+
+  // openFriendList(e) {
+  //   e.preventDefault();
+  //   document.getElementById("friendList").style.height = "200px";
+
+  // }
+
+  // closeFriendList() {
+  //   document.getElementById("friendList").style.height = "0";
+  //   this.setState({ selectedChat: false });
+  // }
+
+  openFriendsList() {
+    const { showFriends } = this.state; 
+    // const { socket } = this.state;
+    // socket.emit('client.inLobby', this.state.store.user);
+    this.setState({ showFriends: !showFriends });
+  }
+
   render() {
-    const { friendsList, selectedChat } = this.state;
+    const { showFriends, friendsList } = this.state;
+    // const { selectedChat } = this.state;
 
     return (
-      // <div>
-      //   <div>
-      //     <div id="friendList" className="friendList container">
-      //       <div className="friendList minimize"><div onClick={() => this.closeFriendList()}>X{' '}</div></div>
-      //       {friendsList.map(friend =>
-      //         (
-      //           <div>
-      //             <div className={`dot ${friend[0]}`} />
-      //             <div className="friend" onClick={() => this.changeSelectedChat(friend[0], friend[1])}>{friend[0]}</div>
-      //           </div>
-      //         ))}
-      //     </div>
-      //   </div>
-      //   <div id="mySidenav" className="sidenav">
-      //     <a href="#" onMouseEnter={e => this.openFriendList(e)}>Friends</a>
-      //   </div>
-      //   <br />
-      //   {selectedChat && <Chat className="hide" friendName={selectedChat[0]} roomID={selectedChat[1]} />}
-      // </div>
-
-    );
+      <View style={{ flex: 1, borderWidth: 1, borderColor: 'green', width: '90%' }}>
+        <View style={{ height: 300 }}>
+            {showFriends && (
+              <ScrollView style={friends.list}>
+              {friendsList.map((friend, index) => 
+                <Text key={index} onPress={() => this.changeSelectedChat(friend[0], friend[1])}>{friend[0]}</Text>       
+              )}
+              </ScrollView>
+            )}
+        </View>
+        <View style={friends.button}>
+          <TouchableOpacity onPress={() => this.openFriendsList()}>
+            <Text style={friends.container}>Friends</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
 
-export default FriendChat;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000000',
+  },
+});
+
+const friends = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#000000',
+  },
+  container: {
+    color: 'yellow',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  friend: {
+    color: 'black',
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  list: {
+    borderWidth: 1,
+    borderColor: 'green',
+    width: 200,
+  }
+});
+
+
+      {/* <View style={{ flex: 1 }}>
+        <View style={{ height: 300 }}>
+        <ScrollView style={friends.list}>
+          {showFriends && friendsList.map( (friend, i) => 
+          <Text style={friends.friend} key={i}>{friend}</Text>)}
+        </ScrollView>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={() => this.updateState()}>
+            <Text style={friends.container}>Friends</Text>
+          </TouchableOpacity>
+        </View>
+      </View> */}
