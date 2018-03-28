d546f2e13b085361a7b3e004f725f6c0d294de86
Kin Chan <xkinchanx@gmail.com>
2018-03-28 15:48:32 -0700

    [update]: refactor to react native with nav

---
+++ client/src/components/home/index.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import $ from 'jquery';
+
+import { AppRegistry, StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import SessionBar from '../navbar/SessionBar';
+
+// import Textarea from '../textarea/Textarea';
+// import Paragraph from '../text/Paragraph';
+// import FriendChat from '../../components/buttons/FriendChat';
+// import RhymeForm from '../rhymeSearch/RhymeForm';
+// import ColorPicker from '../toolbar/ColorPicker';
+// import store from '../../redux/store';
+
+import location from '../../../../config';
+// import './home.css';
+
+// const centerStyle = {
+//   float: 'none',
+//   margin: '0 auto',
+// };
+
+const clickHandler = () => {
+  $.ajax({
+    method: 'POST',
+    url: '/api/content/post',
+    data: {
+      text: $('#lyrics').val(),
+    },
+    success(res) {
+      console.log(res);
+    },
+    error(res) {
+      alert(res); // eslint-disable-line
+    },
+  });
+};
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    let pic = {
+      uri: 'https://i.imgur.com/vWA2TAB.jpg'
+    };
+    return (
+      <View>
+        <SessionBar nav={this.props}/>
+        <View style={styles.image}>
+          <Image source={pic} style={{alignSelf: 'stretch', height: 200}}/>
+        </View>
+        <Text style={{color:'white', textAlign:'center', paddingTop: 10, paddingBottom: 10}}>Compose as you normally would. But be aware: commas signify a word to be rhymed with, as does the end of a line.</Text>
+        <TextInput
+        style={{height: 150, width: 300, alignSelf: 'center', backgroundColor: '#D7D7D7', borderColor: 'gray', borderWidth: 1}}
+        placeholder="Type here..."
+        placeholderTextColor="#333"
+        // onChangeText={(text) => this.setState({text})}
+        // value={this.state.text}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              // onPress={onPressLearnMore}
+              title="Post"
+              color="#333"
+              // accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              // onPress={onPressLearnMore}
+              title="Hit API"
+              color="#333"
+              // accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+// class Home extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = store.getState();
+//     store.subscribe(() => {
+//       this.setState(store.getState());
+//     });
+//   }
+
+//   componentDidMount() {
+//     this.state = store.getState();
+//     console.log(this.state);
+//     store.dispatch({
+//       type: 'browserrestore',
+//       body: {
+//         username: this.state.user ? this.state.user : 'anonymous',
+//       },
+//     });
+//     store.subscribe(() => {
+//       this.setState(store.getState());
+//       // console.log(this.state);
+//     });
+//   }
+
+//   hitHandler = () => {
+//     const strictness = this.state.strictness === 'Strict' ? 3 : 1; // Other possible value is 'loose'.
+//     $.ajax({
+//       method: 'POST',
+//       url: `http://${location}:3001/parse`,
+//       data: {
+//         text: $('#lyrics').val(),
+//         strictness,
+//       },
+//       success(res) {
+//         const colors = JSON.parse(res);
+//         const coords = Object.keys(colors);
+//         store.dispatch({ type: 'wipeboard' });
+//         coords.forEach((coord) => {
+//           store.dispatch({
+//             type: 'straighthighlight',
+//             body: {
+//               coord,
+//               color: colors[coord],
+//             },
+//           });
+//         });
+//       },
+//       error(res) {
+//         alert(res); // eslint-disable-line
+//       },
+//     });
+//   };
+
+//   strictHandler = () => {
+//     if (this.state.strictness === 'Strict') {
+//       store.dispatch({
+//         type: 'changestrictness',
+//         body: {
+//           strictness: 'Loose',
+//         },
+//       });
+//     } else if (this.state.strictness === 'Loose') {
+//       store.dispatch({
+//         type: 'changestrictness',
+//         body: {
+//           strictness: 'Strict',
+//         },
+//       });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       // <div>
+//       //   <div className="jumbotron" />
+//       //   <div className="container-fluid">
+//       //     <div className="row">
+//       //       <div className="col text-center" style={centerStyle} />
+//       //     </div>
+//       //   </div>
+//       //   <div className="row">
+//       //     <Textarea />
+//       //     <Paragraph className="text-center" style={centerStyle} text={this.state.text} />
+//       //   </div>
+//       //   <div className="row">
+//       //     <div className="col-md-6" style={{ margin: '5px' }}>
+//       //       {this.state.session ?
+//       //        'Compose as you normally would. But be aware: commas signify a word to be rhymed with, as does the end of a line.' /* eslint-disable-line */
+//       //        : 'Perhaps you\'d like to sign up...'}
+//       //     </div>
+//       //   </div>
+//       //   {this.state.session &&
+//       //   <div className="row">
+//       //     <div className="col-md-4">
+//       //       <button style={{ margin: '5px' }} className="btn btn-outline-primary" onClick={clickHandler}>Post</button>
+//       //       <button style={{ margin: '5px' }} className="btn btn-outline-primary" onClick={this.hitHandler}>Hit API</button>
+//       //       <button style={{ margin: '5px' }} className="btn btn-outline-primary" onClick={this.strictHandler}>{this.state.strictness}</button>
+//       //     </div>
+//       //     <div className="col-md-2">
+//       //       <span />
+//       //     </div>
+//       //     <div className="col-md-4">
+//       //       {'Color Pad: '}<ColorPicker />
+//       //     </div>
+//       //   </div>}
+//       //   {this.state.session && <FriendChat />}
+//       //   <hr />
+//       //   {this.state.session && <RhymeForm />}
+//       // </div>
+//     );
+//   }
+// }
+
+// export default Home;
+
+
+
+var styles = StyleSheet.create({
+  button: {
+    margin: 10,
+    borderColor: '#D7D7D7',
+    borderWidth: 1
+  },
+  buttonContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  }
+});
--- client/src/components/navbar/SessionBar.js
+++ client/src/components/navbar/SessionBar.js
@@ -1,6 +1,10 @@
 import React from 'react';
+<<<<<<< HEAD
 import {StyleSheet, View, Text} from 'react-native';
 import Menu, {MenuOptions, MenuOption, MenuTrigger} from 'react-native-menu';
+=======
+import { Button, StyleSheet, View, Text } from 'react-native';
+>>>>>>> [update]: refactor to react native with nav
 // import { Link } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import $ from 'jquery';
@@ -21,6 +25,7 @@ import $ from 'jquery';
 //   });
 // };
 
+<<<<<<< HEAD
 const SessionBar = () => (
   <View style={{ paddingTop: 30, paddingBottom: 10, paddingRight: 10, paddingLeft: 10, flexDirection: 'row', backgroundColor: '#ffff64' }}>
   <View style={{ flex: 1}}><Text style={{ fontSize: 18 }}>Rhyme Doctor</Text></View>
@@ -50,6 +55,50 @@ const SessionBar = () => (
     </MenuOptions>
   </Menu>
 </View>
+=======
+const SessionBar = ({ nav }) => (
+  <View style={styles.toolbar}>
+    <Text style={styles.toolbarButton}>Sign Up</Text>
+    <Text style={styles.toolbarTitle}>Rhyme Doctor</Text>
+    <Text style={styles.toolbarButton}>Login</Text>
+    <Button title="Go to Home" onPress={() => nav.navigation.navigate('Home')} />
+    <Button title="Go to Friends" onPress={() => nav.navigation.navigate('Friends')} />
+  </View>
+  // <div>
+  //   <nav id="nav" className="navbar navbar-expand-md navbar-dark bg-dark">
+  //     <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+  //       <ul className="navbar-nav mr-auto">
+  //         <li className="nav-item active">
+  //           <Link className="nav-link" to="/">Home</Link>
+  //         </li>
+  //         <li className="nav-item">
+  //           <Link className="nav-link" to="/feed">Top/News Feed</Link>
+  //         </li>
+  //         <li className="nav-item">
+  //           <Link className="nav-link" to="/profile">Profile</Link>
+  //         </li>
+  //         <li className="nav-item">
+  //           <Link className="nav-link" to="/subscriptions">Subscriptions</Link>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //     <div className="mx-auto order-0">
+  //       <a className="navbar-brand mx-auto" href="#">Rhyme Doctor</a>
+  //       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+  //         <span className="navbar-toggler-icon" />
+  //       </button>
+  //     </div>
+  //     <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+  //       <ul className="navbar-nav ml-auto">
+  //         <li className="nav-item">
+  //           <Link className="nav-link" style={{ display: 'inline' }} to="/about">About</Link>
+  //           <a onClick={logout} style={{ display: 'inline' }} className="nav-link" /* eslint-disable-line */ >Logout</a>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   </nav>
+  // </div>
+>>>>>>> [update]: refactor to react native with nav
 );
 
 var styles = StyleSheet.create({
