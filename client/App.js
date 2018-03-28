3a98d8b9b780259c59e126e953ba880ab5000f8c
Kin Chan <xkinchanx@gmail.com>
2018-03-28 15:49:06 -0700

    [delete]: removed server files + updated App.js

--- client/App.js
+++ client/App.js
@@ -1,52 +1,31 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { StackNavigator } from 'react-navigation';
+import Home from './src/components/home/index';
 import SessionBar from './src/components/navbar/SessionBar';
 
 import FriendChat from './src/components/buttons/FriendChat';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+    Friends: {
+      screen: FriendChat,
+    },
+  },
+  {
+    initialRouteName: 'Home',
   }
+)
+
+export default class App extends React.Component {
   render() {
-    let pic = {
-      uri: 'https://i.imgur.com/vWA2TAB.jpg'
-    };
     return (
       <View style={styles.container}>
-        <SessionBar />
-          <View style={styles.image}>
-            <Image source={pic} style={{alignSelf: 'stretch', height: 200}}/>
-          </View>
-        <Text style={{color:'white', textAlign:'center', paddingTop: 10, paddingBottom: 10}}>Compose as you normally would. But be aware: commas signify a word to be rhymed with, as does the end of a line.</Text>
-        <TextInput
-          style={{height: 150, width: 300, alignSelf: 'center', backgroundColor: '#D7D7D7', borderColor: 'gray', borderWidth: 1}}
-          placeholder="Type here..."
-          placeholderTextColor="#333"
-        // onChangeText={(text) => this.setState({text})}
-        // value={this.state.text}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button
-              // onPress={onPressLearnMore}
-              title="Post"
-              color="#333"
-              // accessibilityLabel="Learn more about this purple button"
-            />
-          </View>
-          <View style={styles.button}>
-            <Button
-              // onPress={onPressLearnMore}
-              title="Hit API"
-              color="#333"
-              // accessibilityLabel="Learn more about this purple button"
-            />
-          </View>
-        </View>
-        <FriendChat />
-        
+        <RootStack />
       </View>
     );
   }
