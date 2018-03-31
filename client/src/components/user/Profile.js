a4211738ece20773157532a30e15c2b1806e9631
elaskowski <evalaskows@gmail.com>
2018-03-31 10:36:06 -0700

    Styling on Profile

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { AppRegistry, StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 import SessionBar from '../navbar/SessionBar';
 // import UserPosts from './UserPosts';
 // import Stats from './Stats';
@@ -32,7 +32,7 @@ export default class Profile extends React.Component {
     //   this.getUserPosts(username);
     // } else {
       this.getUserData();
-      // this.getUserPosts();
+      this.getUserPosts();
     // }
   }
 
@@ -44,10 +44,6 @@ export default class Profile extends React.Component {
   // }
 
   getUserData = (username) => {
-    console.log(`http://${location}:3421/api/profile`);;
-    // console.log
-    // onst userData = axios.get('http://localhost:3000/api/profile');
-      // const userData = username ? axios.get('http://localhost:3000/api/profile', { params: { name: username } }) : axios.get('http://192.168.1.11:3000/api/profile');
       axios.get(`http://${location}:3421/api/profile`)
         .then((res) => {
           this.setState({
@@ -79,16 +75,17 @@ export default class Profile extends React.Component {
   //   }
   // }
 
-  // getUserPosts = async (username) => {
-  //   try {
-  //     const userPosts = username ? await axios.get('api/profile/posts', { params: { name: username } }) : await axios.get('api/profile/posts');
-  //     this.setState({
-  //       userPosts: userPosts.data,
-  //     });
-  //   } catch (err) {
-  //     console.log('Failed to get user posts');
-  //   }
-  // }
+  getUserPosts = async (username) => {
+    try {
+      const userPosts = username ? await axios.get(`http://${location}:3421/api/profile/posts`, { params: { name: username } }) : await axios.get(`http://${location}:3421/api/profile/posts`);
+      this.setState({
+        userPosts: userPosts.data,
+      });
+      // console.log(userPosts);
+    } catch (err) {
+      console.log('Failed to get user posts');
+    }
+  }
 
   render() {
     // const { state } = this.props.location;
@@ -98,51 +95,32 @@ export default class Profile extends React.Component {
     let name = 'meowskers';
     let bio = 'Born and raised in Mckinney, TX. I have the best rhymes in the game so yours are probably lame'
     return (
-      <View style={styles.container}>
-        <SessionBar nav={this.props}/>
-        <View style={styles.innerContainer}>
-          <View style={styles.topContainer}>
-            <View style={styles.image}>
-              <Image source={this.state.image} style={{height: 100, width: 100}}/>
+        <View style={styles.container}>
+          <SessionBar nav={this.props}/>
+          <View style={styles.innerContainer}>
+            <View style={styles.topContainer}>
+              <View style={styles.image}>
+                <Image source={this.state.image} style={{height: 100, width: 100}}/>
+              </View>
+              <View style={styles.bio}>
+                <Text style={{color:'white', fontSize:20, fontWeight: 'bold', marginLeft: 65}}>{this.state.username}</Text>
+                <Text style={{color:'white', fontSize:16, fontWeight: 'bold', marginLeft: 85}}>Likes: {this.state.likeCount}</Text>
+              </View>
             </View>
-            <View style={styles.bio}>
-              <Text style={{color:'white', fontSize:20, fontWeight: 'bold', marginLeft: 65}}>{this.state.username}</Text>
-              <Text style={{color:'white', fontSize:16, fontWeight: 'bold', marginLeft: 85}}>Likes: {this.state.likeCount}</Text>
+            <View style={{marginTop: 10}}>
+              {/* <Text style={{color:'white', fontSize:14, fontWeight: 'bold', marginLeft: 10}}>About Me</Text> */}
+              <Text style={{color:'white', fontSize:14, marginLeft: 10}}>{this.state.bio}</Text>
+              {/* <Text>{this.state.userPosts.length}</Text> */}
             </View>
+            <View
+              style={{
+                borderBottomColor: '#686868',
+                borderBottomWidth: 1,
+                marginTop: 15
+              }}
+            />
           </View>
-          <View style={{marginTop: 10}}>
-            {/* <Text style={{color:'white', fontSize:14, fontWeight: 'bold', marginLeft: 10}}>About Me</Text> */}
-            <Text style={{color:'white', fontSize:14, marginLeft: 10}}>{this.state.bio}</Text>
-          </View>
-          {/* <Text style={{marginTop: 200, alignSelf: 'center', fontSize: 20, fontWeight:'bold', color:'white'}}>Posts go here</Text> */}
-          {/* <Text style={{color:'white', textAlign:'center', paddingTop: 10, paddingBottom: 10}}>Compose as you normally would. But be aware: commas signify a word to be rhymed with, as does the end of a line.</Text> */}
-          {/* <TextInput
-            style={{height: 150, width: 300, alignSelf: 'center', backgroundColor: '#D7D7D7', borderColor: 'gray', borderWidth: 1}}
-            placeholder="Type here..."
-            placeholderTextColor="#333"
-          // onChangeText={(text) => this.setState({text})}
-          // value={this.state.text}
-          />
-          <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-              <Button
-                // onPress={onPressLearnMore}
-                title="Post"
-                color="#333"
-                // accessibilityLabel="Learn more about this purple button"
-              />
-            </View>
-            <View style={styles.button}>
-              <Button
-                // onPress={onPressLearnMore}
-                title="Hit API"
-                color="#333"
-                // accessibilityLabel="Learn more about this purple button"
-              />
-            </View> */}
-          {/* </View> */}
         </View>
-      </View>
     );
   }
 }
