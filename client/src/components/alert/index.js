0d9653fb1cc3c037f58784d15c6d60df5d977dab
Kin Chan <xkinchanx@gmail.com>
2018-03-31 14:42:54 -0700

    [add]: alert styling

--- client/src/components/alert/index.js
+++ client/src/components/alert/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const Alert = (props) => {
   return (
-    <Text>{props.message}</Text>
+    <View style={styles.main}>
+      <Text style={styles.text}>{props.message}</Text>
+    </View>
   );
 };
 
 export default Alert;
+
+const styles = StyleSheet.create({
+  ...Platform.select({
+    ios: {
+      main: {
+        backgroundColor: '#fff3cd',
+        borderRadius: 5,
+        height: 75,
+        width: '100%',
+        alignItems: 'center',
+      },
+      text: {
+        color: '#856404',
+        fontSize: 20,
+        fontWeight: '700',
+        paddingTop: 25,
+      }
+    },
+    android: {
+      main: {
+        backgroundColor: '#fff3cd',
+        borderRadius: 5,
+        height: 75,
+        width: '100%',
+        alignItems: 'center',
+      },
+      text: {
+        color: '#856404',
+        fontSize: 20,
+        fontWeight: '700',
+        paddingTop: 25,
+      }
+    }
+  })
+})
