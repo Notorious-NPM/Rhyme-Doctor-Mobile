2dffc8418a347718a9f024764edf395b7485a10d
elaskowski <evalaskows@gmail.com>
2018-04-02 18:13:31 -0700

    Images pulling and loading from Camera Roll

--- client/src/components/user/ViewPhotos.js
+++ client/src/components/user/ViewPhotos.js
@@ -6,16 +6,48 @@ import {
   ListView,
   StyleSheet,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  ScrollView
 } from 'react-native';
 
-export default class ViewPhotos extends Component {
+export default class ViewPhotos extends React.Component {
   constructor(props) {
     super(props)
   }
+
   render() {
-    return (<View>
-      <Text>{this.props.photos === undefined ? null : this.props.photos}</Text>
-    </View>)
+    return <ScrollView>{this.props.photos.map((p, i) => {
+      return(
+        <Image
+          key={i}
+          style={{
+            width: 300,
+            height: 100
+          }}
+          source={{ uri: p.node.image.uri }}
+        />
+      )
+    })}</ScrollView>
   }
 }
+
+// return (
+//   <View>
+//     <Text>{this.props.photos === undefined ? null : this.props.photos.length}</Text>
+//   </View>
+// )
+//   return ({<ScrollView>
+//     {this.props.photos === undefined 
+//       ? null 
+//       : this.props.photos.map((p, i) => {
+//         <Image
+//           key={i}
+//           style={{
+//             width: 300,
+//             height: 100
+//           }}
+//           source={{ uri: p.node.image.uri }}
+//         />
+//       })}
+//   </ScrollView>)
+// }
