4d2f0a89049113d937cb8d67216b43c3eef74454
elaskowski <evalaskows@gmail.com>
2018-04-04 11:55:03 -0700

    Updated view photos to modal

--- client/src/components/user/ViewPhotos.js
+++ client/src/components/user/ViewPhotos.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-import { Image, View, ListView, StyleSheet, Text, TouchableHighlight, ScrollView, Modal} from 'react-native';
+import { Image, View, ListView, StyleSheet, Text, TouchableHighlight, ScrollView, Modal, Alert } from 'react-native';
+
+import API_KEY from './config';
 
 export default class ViewPhotos extends React.Component {
   constructor(props) {
@@ -17,6 +20,52 @@ export default class ViewPhotos extends React.Component {
     this.props.hideModal();
   }
 
+  handleDrop = (file) => {
+    // console.log(files);
+    // const uploaders = (file) => {
+      const formData = new FormData(); // eslint-disable-line
+      formData.append('file', file);
+      formData.append('upload_preset', 'hkhkmnpg');
+      formData.append('api_key', API_KEY);
+      formData.append('timestamp', (Date.now() / 1000) || 0);
+
+      // return axios.post('https://api.cloudinary.com/v1_1/dkwbeount/image/upload', JSON.stringify(formData), {
+      //   headers: { 'content-type': 'application/json' },
+      // }).then((response) => {
+      //   // const { data } = response;
+      //   // const fileURL = data.secure_url;
+      //   console.log('res', JSON.parse(response));
+      //   // axios.put('api/profile/image', { image: fileURL });
+      //   // this.setState({
+      //   //   image: fileURL,
+      //   // });
+      // }).catch(err => console.log(err))
+
+      let apiUrl = 'https://api.cloudinary.com/v1_1/dkwbeount/image/upload';
+
+      let data = {
+        "file": file,
+        "upload_preset": 'hkhkmnpg'
+      }
+
+      fetch(apiUrl, {
+        body: JSON.stringify(data),
+        headers: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+      }).then(r=> {
+        console.log(r);
+      }).catch(err => console.log(err))
+
+    // axios.all(uploaders).then(() => {
+    //   // this.setState({
+    //   //   showChangePic: false,
+    //   // });
+    //   console.log('done', this.state);
+    // });
+  }
+
   render() {
     return (
       <View style={{margin:30}}>
@@ -27,7 +76,7 @@ export default class ViewPhotos extends React.Component {
           presentationStyle="pageSheet"
           visible={this.state.modalVisible}
           onRequestClose={() => {
-            alert('Modal has been closed.');
+            alert('Modal has been closed.'); //change this
           }}>
           <View style={{backgroundColor: '#333'}}>
           <TouchableHighlight
@@ -45,17 +94,28 @@ export default class ViewPhotos extends React.Component {
           </TouchableHighlight>
           <ScrollView style= {{margin: 30}}>{this.props.photos.map((p, i) => {
             return(
-              <Image
+              <TouchableHighlight 
                 key={i}
-                style={{
-                  width: 300,
-                  height: 300,
-                  marginTop: 10,
-                  borderColor: 'white',
-                  borderWidth: 2
-                }}
-                source={{ uri: p.node.image.uri }}
-              />
+                onPress={() => Alert.alert(
+                  'Change profile picture?',
+                  '',
+                  [
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed')},
+                    {text: 'OK', onPress: () => this.handleDrop(p.node.image.uri)}
+                  ],
+                  // { cancelable: true }
+              )}>
+                <Image
+                  style={{
+                    width: 300,
+                    height: 300,
+                    marginTop: 10,
+                    borderColor: 'white',
+                    borderWidth: 2
+                  }}
+                  source={{ uri: p.node.image.uri }}
+                  />
+                </TouchableHighlight>
             )
           })}
           </ScrollView>
