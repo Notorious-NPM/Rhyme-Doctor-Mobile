6d4dd94a2d7ceea3a9e5342041fa43785e49a365
Kin Chan <xkinchanx@gmail.com>
2018-04-04 17:35:19 -0700

    [update]: end message

--- client/src/components/rap-post/LoopMsg.js
+++ client/src/components/rap-post/LoopMsg.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default LoopMsg = () => (
-  <View style={styles.main}>
-    <Text>
-      You have reached the end so we brought you back to the beginning
-    </Text>
+  <View style={{ flex: 1, alignItems: 'center' }}>
+    <View style={styles.main}>
+      <Text style={styles.text}>You have reached the end</Text>
+      <Text style={styles.text}>visit https://rhymedoctor.fun for more fun!</Text>
+    </View>
   </View>
 )
 
@@ -13,14 +14,31 @@ const styles = StyleSheet.create({
   ...Platform.select({
     ios: {
       main: {
-        backgroundColor: '#D1D1D1',
+        backgroundColor: 'rgba(168, 168, 168, 0.2)',
         paddingTop: 5,
         paddingBottom: 5,
-        borderRadius: 10,
+        borderRadius: 15,
+        width: '80%',
+        alignItems: 'center',
+      },
+      text: {
+        fontWeight: '800',
+        color: '#E6E4E4'
       }
     },
     android: {
-
+      main: {
+        backgroundColor: 'rgba(168, 168, 168, 0.2)',
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 15,
+        width: '80%',
+        alignItems: 'center',
+      },
+      text: {
+        fontWeight: '800',
+        color: '#E6E4E4'
+      }
     }
   })
 
